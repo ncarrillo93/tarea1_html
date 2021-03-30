9a45f59cc8e53cb4652fc2d027967f6b41bda8ec
ncarrillo93 <nicolascarrillo33@gmail.com>
2021-03-30 16:24:24 -0300

    formulario listo. falta mostrar datos en tabla

---
+++ formulario.php
@@ -0,0 +1,8 @@
+<?php
+$nombre = $_GET['nombre'];
+$correo = $_GET['correo'];
+$fechaNacimiento = $_GET['fechaNacimiento'];
+$genero = $_GET['genero'];
+$carrera = $_GET['carrera'];
+echo "<h2> " $nombre "</h2>"
+?>
---
+++ funciones.js
@@ -0,0 +1,7 @@
+function getData(){
+    var nombre = document.getElementById('nombre').value;
+    var correo = document.getElementById('correo').value;
+    var fechaNacimiento = document.getElementById('fechaNacimiento').value;
+    var genero = document.getElementById('genero').value;
+    var carrera = document.getElementById('carrera').value;
+} 
